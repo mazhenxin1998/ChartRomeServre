dc5956c53b35ac4ca1e5dd7fc67f91606a9dd072
mazhenxin1998 <2280480546@qq.com>
2020-01-30 18:16:45 +0800

    init

---
+++ src/test/java/com/mzx/ChartserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
